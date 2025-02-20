ca1b5333602875355ead65a3a03263c829652f14
Bahodir <bahodir.media@gmail.com>
2025-02-20 18:56:10 +0500

    Арифметически операторы

---
+++ 08/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Learn PHP 8 - Lesson 8</title>
+</head>
+
+<body>
+	<?php
+	
+	// var_dump(+'10.1'); // float(10.1)	
+	// var_dump(1 + 3); // int(4)
+	// var_dump(1 - 2); // int(-1)	
+	// var_dump(2 * 3); // int(6)	
+	// var_dump(10 / 2); // int(5)
+	
+	var_dump(1 + '2test'); // int(3)
+	var_dump(1 + '2'); // int(3)
+	
+	var_dump(20 % 8); // int(4)
+	var_dump(2 ** 4); // int(16) 2 * 2 * 2 * 2 = 16
+	
+	?>
+
+</body>
+
+</html>
